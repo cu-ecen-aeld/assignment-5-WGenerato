a751490ec5122189984f54e7644989977ef6e9a1
WGenerato <vnk3897@gmail.com>
2024-06-16 11:37:05 -0700

    Build successfully

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,17 +1,6 @@
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-WGenerato.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
+# base_external/external.mk
+BR2_EXTERNAL_PACKAGES += aesd-assignments
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)
-endef
+# Include the package makefile with absolute path
+include /home/tt20/aesd-assignment-4/base_external/package/aesd-assignments/aesd-assignments.mk
 
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
-    $(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-endef
-
-$(eval $(generic-package))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,28 +4,34 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '40db0fd9e99ddf1709f68d7b46a61a6f09193cbc'
+# Fill up the contents below in order to reference your assignment 3 git contents
+AESD_ASSIGNMENTS_VERSION = b28c66832be9491e02d2ab6ffb603edd80b594f7   
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-WGenerato.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-WGenerato.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-AESD_ASSIGNMENTS_DIR = $(TOPDIR)/../..
-
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+         @echo "CROSS_COMPILE=$(CROSS_COMPILE)"
+	 $(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) CROSS_COMPILE=$(TARGET_CROSS)
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 	$(INSTALL) -d $(TARGET_DIR)/etc/init.d/
-	$(INSTALL) -m 0755 $(AESD_ASSIGNMENTS_DIR)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+endef
+
+# Fetch the repository and checkout the specified commit
+define AESD_ASSIGNMENTS_PREPARE_SOURCE
+    git clone $(AESD_ASSIGNMENTS_SITE) $(@D)
+    cd $(@D) && git checkout $(AESD_ASSIGNMENTS_VERSION)
 endef
 
 $(eval $(generic-package))
+

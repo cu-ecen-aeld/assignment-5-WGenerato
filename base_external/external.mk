dc587e2b928289b75f31bf7ba782c70ef2951c99
WGenerato <vnk3897@gmail.com>
2024-06-17 02:08:48 -0700

    affirmative actions

--- base_external/external.mk
+++ base_external/external.mk
@@ -4,6 +4,9 @@ BR2_EXTERNAL_PACKAGES += aesd-assignments
 # Include the package makefile with a relative path
 include $(BR2_EXTERNAL)/package/aesd-assignments/aesd-assignments.mk
 
+# Add the defconfig file to the configuration path
+BR2_DEFCONFIG = $(BR2_EXTERNAL)/configs/aesd_qemu_defconfig
+
 # Specify the Config.in path
 BR2_EXTERNAL_CONFIG += $(BR2_EXTERNAL)/package/aesd-assignments/Config.in
 

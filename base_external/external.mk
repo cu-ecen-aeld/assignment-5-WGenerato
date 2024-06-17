bc6ae7ba661f3d4b6f7c6100e7d46fe90b79b592
WGenerato <vnk3897@gmail.com>
2024-06-17 02:00:59 -0700

    affirmative actions

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,6 +1,9 @@
 # base_external/external.mk
 BR2_EXTERNAL_PACKAGES += aesd-assignments
 
-# Include the package makefile with absolute path
-include /home/tt20/aesd-assignment-4/base_external/package/aesd-assignments/aesd-assignments.mk
+# Include the package makefile with a relative path
+include $(BR2_EXTERNAL)/package/aesd-assignments/aesd-assignments.mk
+
+# Specify the Config.in path
+BR2_EXTERNAL_CONFIG += $(BR2_EXTERNAL)/package/aesd-assignments/Config.in
 

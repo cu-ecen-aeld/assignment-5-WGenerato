e696ba4197e5b53bc0d70d49cb6a3df83d04c183
WGenerato <vnk3897@gmail.com>
2024-06-23 03:48:23 -0700

    last last last

--- base_external/external.mk
+++ base_external/external.mk
@@ -3,8 +3,8 @@
 BR2_EXTERNAL_PACKAGES += aesd-assignments
 
 # Include the package makefile with relative path
-include /home/tt20/aesd-assignment-4/base_external/package/aesd-assignments/aesd-assignments.mk
+include $(GITHUB_WORKSPACE)/base_external/package/aesd-assignments/aesd-assignments.mk
 
 # Add the defconfig file to the configuration path
-BR2_DEFCONFIG = /home/tt20/aesd-assignment-4/base_external/configs/aesd_qemu_defconfig
+BR2_DEFCONFIG = $(GITHUB_WORKSPACE)/base_external/configs/aesd_qemu_defconfig
 

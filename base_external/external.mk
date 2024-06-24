a5e46690bdd7a7cc32c99a5af11592fd4d5510d4
WGenerato <vnk3897@gmail.com>
2024-06-24 10:36:10 -0700

    true last

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,3 +1,3 @@
-# base_external/external.mk
+
 include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
 
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -14,14 +14,15 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL)  -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -d $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
